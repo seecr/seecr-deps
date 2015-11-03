a832b99b0487679574d112f652c14db6974f7d31
Johan Jonkers <johan@seecr.nl>
2015-11-03 15:20:13 +0100

    JJ: scanforDeps

--- seecrdeps/__init__.py
+++ seecrdeps/__init__.py
@@ -21,17 +21,29 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 ## end license ##
-from os.path import abspath, dirname, isdir, join
+from os.path import abspath, dirname, isdir, join, isfile
 from glob import glob
 
-def includeParentAndDeps(filename, systemPath=None, additionalPaths=None, additionalPathsRelativeFromParent=False):
+def includeParentAndDeps(filename, systemPath=None, additionalPaths=None, scanForDeps=False, additionalPathsRelativeFromParent=False):
     if systemPath is None:
         from sys import path as systemPath
+
+    def hasDeps(d):
+        return isfile(join(d, "deps.txt")) or isdir(join(d, "deps.d"))
+
     parentDirectory = dirname(dirname(abspath(filename)))
+    if scanForDeps:
+        while not hasDeps(parentDirectory):
+            parentDirectory = dirname(parentDirectory)
+            if parentDirectory == "/":
+                parentDirectory = dirname(dirname(abspath(filename)))
+                break
+
     depsDirectory = join(parentDirectory, "deps.d")
     if isdir(depsDirectory):
-        list(map(lambda path: systemPath.insert(0, path), glob(join(depsDirectory, "*"))))
+        map(lambda path: systemPath.insert(0, path), glob(join(depsDirectory, "*")))
     systemPath.insert(0, parentDirectory)
+
     if additionalPaths:
         list(map(
             lambda path: systemPath.insert(0, join(parentDirectory, path) if additionalPathsRelativeFromParent else path), 
